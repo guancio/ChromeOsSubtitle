7b4c7e6962bf0deb43815b3512018647a2cae126
Roberto Guanciale <guancio@gmail.com>
2013-10-19 12:05:52 +0000

    moved out and fixed style of open button

--- src/js/app.js
+++ src/js/app.js
@@ -2,38 +2,6 @@ var myURL = window.URL || window.webkitURL;
 
 var packaged_app = (window.location.origin.indexOf("chrome-extension") == 0);
 
-(function($) {
-    $.extend(MediaElementPlayer.prototype, {
-	buildsource: function(player, controls, layers, media) {
-	    var 
-	    t = this,
-	    openFileInput = $('<input style="display:none" type="file" id="openfile_input"/>')
-		.appendTo(controls);
-	    t.openedFile = null;
-	    var open  = 
-		$('<div class="mejs-button mejs-source-button mejs-source" >' +
-		  '<button type="button" aria-controls="' + t.id + '" title="' + mejs.i18n.t('Open video...') + '" aria-label="' + mejs.i18n.t('Open video...') + '"></button>' +
-		  '</div>')
-		.appendTo(controls);
-	    player.openFileForm = function () {
-		openFileInput[0].click();
-	    };
-	    open.click(function(e) {
-		e.preventDefault();
-		player.openFileForm();
-		return false;
-	    });
-	    openFileInput.change(function (e) {
-		mainMediaElement.stop();
-		player.tracks = [];
-		var path = window.URL.createObjectURL(openFileInput[0].files[0]);
-		t.openedFile = openFileInput[0].files[0];
-		mainMediaElement.setSrc(path);
-		return false;
-	    });
-	}
-    });
-})(mejs.$);
 
 
 (function($) {
