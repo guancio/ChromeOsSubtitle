e6335de55f00f4b30b3a3c3c4d9630438fca80e0
Roberto Guanciale <guancio@gmail.com>
2013-11-10 21:24:46 +0000

    typo

--- src/js/app.js
+++ src/js/app.js
@@ -62,10 +62,10 @@ var packaged_app = (window.location.origin.indexOf("chrome-extension") == 0);
 	    infoText = infoText + 'You can donate to this project via <a href="https://flattr.com/submit/auto?user_id=guancio&url=https://github.com/guancio/ChromeOsSubtitle&title=ChromeOsSubtitle&language=&tags=github&category=software"><img src="'+mediaelement_url+'flattr.png"></a><br><br>';
 
 	    infoText = infoText +
-		'This software is possible thanks to several open source projects:<ul>'+
+		'This software is possible thank to several open source projects:<ul>'+
 		'<li>The main madia player component is a fork of <a id="link_mediaelement" href="http://mediaelementjs.com/">MediaelEment.js</a>, developed by John Dyer</li>'+
 		'<li>Zip files are opened using <a href="http://gildas-lormeau.github.io/zip.js/" target="_blank">zip.js</a></li>';
-	    infoText = infoText + '<li>Subtitles service powered by <a href="http://www.OpenSubtitles.org" target="_blank">www.OpenSubtitles.org</a>. More uploaded subs means more subs available. Please opload <a href="http://www.opensubtitles.org/upload" target="_blank">here</a> jour subs.<br/><a href="http://www.OpenSubtitles.org" target="_blank"><img src="'+mediaelement_url+'opensubtitle.gif"/></a></li>';
+	    infoText = infoText + '<li>Subtitles service powered by <a href="http://www.OpenSubtitles.org" target="_blank">www.OpenSubtitles.org</a>. More uploaded subs means more subs available. Please upload <a href="http://www.opensubtitles.org/upload" target="_blank">here</a> jour subs.<br/><a href="http://www.OpenSubtitles.org" target="_blank"><img src="'+mediaelement_url+'opensubtitle.gif"/></a></li>';
 	    infoText = infoText + '</ul>[Click the box to close the info window]</div>'
 
 	    var info = $(infoText
