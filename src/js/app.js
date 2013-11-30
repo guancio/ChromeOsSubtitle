c6ca062b9912ba2ca7907de2d025a0ae921b566e
Roberto Guanciale <guancio@gmail.com>
2013-11-30 22:36:13 +0000

    started prototype to automatically open srt files

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,8 @@ $('#main').append('<video id="player" controls="controls"></video>');
 
 var features = ['source', 'settings','playpause','progress','current','duration', 'tracks','subdelay', 'subsize', 'volume', 'settingsbutton', 'info', 'help', 'fullscreen', 'drop', 'stats'];
 features.push('opensubtitle');
+if (packaged_app)
+    features.push('autosrt');
 
 $('#player').mediaelementplayer({
     startLanguage:'en',
