f81b59fdf19d795d4daef66268267e0372c9c72d
Vivek Kannan <vivek7641@gmail.com>
2016-08-02 19:10:50 +0530

    Update playlist.js

--- src/js/features/playlist.js
+++ src/js/features/playlist.js
@@ -1,8 +1,50 @@
-(function($) {
-    MediaElementPlayer.prototype.buildplaylist = function(player, controls, layers, media) {
-            if(!player.isVideo)
+(function() {
+    var playTypes = ['Normal', 'Repeat', 'Shuffle'],
+        playType = 0;
+    
+    MediaElementPlayer.prototype.buildplaylist = function() {
+        this.playlist = [];
+        this.playIndex = null;
+    };
+    
+    MediaElementPlayer.prototype.next = function() {
+        if(playType === 0) {
+            if(this.playIndex === this.playlist.length - 1) {
                 return;
-                
-            // add speed controls
-    }
-})(mejs.$);
+            }
+            
+            this.playIndex++;
+        }
+        else if(playType === 1) {
+            this.playIndex = (this.playIndex + 1) % this.playlist.length;
+        }
+        else {
+            this.playIndex = Math.floor(Math.random() * this.playlist.length);
+        }
+        
+        this.setSrc(this.playlist[this.playIndex]);
+    };
+    
+    MediaElementPlayer.prototype.previous = function() {
+        if(playType === 0) {
+            if(this.playIndex === 0) {
+                return;
+            }
+            
+            playIndex--;
+        }
+        else if(playType === 1) {
+            this.playIndex = (this.playlist.length + this.playIndex - 1) % this.playlist.length;
+        }
+        else {
+            this.playIndex = Math.floor(Math.random() * this.playlist.length);
+        }
+        
+        this.setSrc(this.playlist[this.playIndex]);
+    };
+    
+    MediaElementPlayer.prototype.changePlayType = function() {
+        playType = (playType + 1) % 3;
+        this.setNotification('Playlist Navigation: ' + playTypes[playType]);
+    };
+})();
