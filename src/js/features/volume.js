88e6aa780d9d89230aa1fcd7d35ccdd3c5f15cb5
vivekannan <vivek7641@gmail.com>
2016-08-03 14:57:41 +0530

    Removed redundant code from volume.js

--- src/js/features/volume.js
+++ src/js/features/volume.js
@@ -2,9 +2,6 @@
     $.extend(mejs.MepDefaults, {
         muteText: mejs.i18n.t('Mute Toggle'),
         hideVolumeOnTouchDevices: true,
-        
-        audioVolume: 'horizontal',
-        videoVolume: 'vertical'
     });
     
     MediaElementPlayer.prototype.buildvolume = function() {
@@ -13,23 +10,7 @@
             return;
         
         var t = this,
-            mode = (t.isVideo) ? t.options.videoVolume : t.options.audioVolume,
-            mute = (mode == 'horizontal') ?
-            
-            // horizontal version
-            $('<div class="mejs-button mejs-volume-button mejs-mute">' +
-                '<button type="button" title="' + t.muteText + '" aria-label="' + t.muteText + '"></button>' +
-                '</div>' +
-                '<div class="mejs-horizontal-volume-slider">' + // outer background
-                '<div class="mejs-horizontal-volume-total"></div>' + // line background
-                '<div class="mejs-horizontal-volume-current"></div>' + // current volume
-                '<div class="mejs-horizontal-volume-handle"></div>' + // handle
-                '</div>'
-            )
-            .appendTo(t.controls) :
-            
-            // vertical version
-            $('<div class="mejs-button mejs-volume-button mejs-mute">' +
+            mute = $('<div class="mejs-button mejs-volume-button mejs-mute">' +
                 '<button type="button" title="' + t.options.muteText + '" aria-label="' + t.options.muteText + '"></button>' +
                 '<div class="mejs-volume-slider">' + // outer background
                 '<div class="mejs-volume-total"></div>' + // line background
@@ -38,10 +19,10 @@
                 '</div>' +
                 '</div>')
             .appendTo(t.controls),
-            volumeSlider = t.container.find('.mejs-volume-slider, .mejs-horizontal-volume-slider'),
-            volumeTotal = t.container.find('.mejs-volume-total, .mejs-horizontal-volume-total'),
-            volumeCurrent = t.container.find('.mejs-volume-current, .mejs-horizontal-volume-current'),
-            volumeHandle = t.container.find('.mejs-volume-handle, .mejs-horizontal-volume-handle'),
+            volumeSlider = t.container.find('.mejs-volume-slider'),
+            volumeTotal = t.container.find('.mejs-volume-total'),
+            volumeCurrent = t.container.find('.mejs-volume-current'),
+            volumeHandle = t.container.find('.mejs-volume-handle'),
             
             positionVolumeHandle = function(volume, secondTry) {
                 if(!volumeSlider.is(':visible') && typeof secondTry == 'undefined') {
@@ -62,67 +43,40 @@
                 }
                 
                 // position slider 
-                if(mode == 'vertical') {
-                    var
+                
+                var
+                
+                // height of the full size volume slider background
+                    totalHeight = volumeTotal.height(),
                     
-                    // height of the full size volume slider background
-                        totalHeight = volumeTotal.height(),
-                        
-                        // top/left of full size volume slider background
-                        totalPosition = volumeTotal.position(),
-                        
-                        // the new top position based on the current volume
-                        // 70% volume on 100px height == top:30px
-                        newTop = totalHeight - (totalHeight * volume);
-                        
-                    // handle
-                    volumeHandle.css('top', Math.round(totalPosition.top + newTop - (volumeHandle.height() / 2)));
+                    // top/left of full size volume slider background
+                    totalPosition = volumeTotal.position(),
                     
-                    // show the current visibility
-                    volumeCurrent.height(totalHeight - newTop);
-                    volumeCurrent.css('top', totalPosition.top + newTop);
-                } else {
-                    var
-                    
-                    // height of the full size volume slider background
-                        totalWidth = volumeTotal.width(),
-                        
-                        // top/left of full size volume slider background
-                        totalPosition = volumeTotal.position(),
-                        
-                        // the new left position based on the current volume
-                        newLeft = totalWidth * volume;
-                        
-                    // handle
-                    volumeHandle.css('left', Math.round(totalPosition.left + newLeft - (volumeHandle.width() / 2)));
-                    
-                    // rezize the current part of the volume bar
-                    volumeCurrent.width(Math.round(newLeft));
-                }
+                    // the new top position based on the current volume
+                    // 70% volume on 100px height == top:30px
+                    newTop = totalHeight - (totalHeight * volume);
+                
+                // handle
+                volumeHandle.css('top', Math.round(totalPosition.top + newTop - (volumeHandle.height() / 2)));
+                
+                // show the current visibility
+                volumeCurrent.height(totalHeight - newTop);
+                volumeCurrent.css('top', totalPosition.top + newTop);
             },
             handleVolumeMove = function(e) {
-            
                 var volume = null,
                     totalOffset = volumeTotal.offset();
                     
                 // calculate the new volume based on the moust position
-                if(mode == 'vertical') {
-                    var railHeight = volumeTotal.height(),
-                        totalTop = parseInt(volumeTotal.css('top').replace(/px/, ''), 10),
-                        newY = e.pageY - totalOffset.top;
-                        
-                    volume = (railHeight - newY) / railHeight;
+                var railHeight = volumeTotal.height(),
+                    totalTop = parseInt(volumeTotal.css('top').replace(/px/, ''), 10),
+                    newY = e.pageY - totalOffset.top;
                     
-                    // the controls just hide themselves (usually when mouse moves too far up)
-                    if(totalOffset.top == 0 || totalOffset.left == 0)
-                        return;
-                        
-                } else {
-                    var railWidth = volumeTotal.width(),
-                        newX = e.pageX - totalOffset.left;
-                        
-                    volume = newX / railWidth;
-                }
+                volume = (railHeight - newY) / railHeight;
+                
+                // the controls just hide themselves (usually when mouse moves too far up)
+                if(totalOffset.top == 0 || totalOffset.left == 0)
+                    return;
                 
                 // ensure the volume isn't outside 0-1
                 volume = Math.max(0, Math.min(volume, 1));
@@ -131,7 +85,7 @@
                 positionVolumeHandle(volume);
                 
                 // set the media object (this will trigger the volumechanged event)
-                volume === 0 ? t.setMuted(true) : t.setMuted(false);
+                t.setMuted(volume === 0);
                 t.setVolume(volume);
             },
             mouseIsDown = false,
@@ -139,20 +93,18 @@
             
         // SLIDER
         
-        mute
-            .hover(function() {
+        mute.hover(function() {
                 volumeSlider.show();
                 mouseIsOver = true;
             }, function() {
                 mouseIsOver = false;
                 
-                if(!mouseIsDown && mode == 'vertical') {
+                if(!mouseIsDown) {
                     volumeSlider.hide();
                 }
             });
             
-        volumeSlider
-            .bind('mouseover', function() {
+        volumeSlider.bind('mouseover', function() {
                 mouseIsOver = true;
             })
             .bind('mousedown', function(e) {
@@ -164,7 +116,7 @@
                     mouseIsDown = false;
                     t.globalUnbind('.vol');
                     
-                    if(!mouseIsOver && mode == 'vertical') {
+                    if(!mouseIsOver) {
                         volumeSlider.hide();
                     }
                 });
@@ -181,7 +133,7 @@
         // listen for volume change events from other sources
         t.media.addEventListener('volumechange', function(e) {
             if(!mouseIsDown) {
-                if(t.isMuted()) {
+                if(t.isMuted() || t.getVolume() === 0) {
                     positionVolumeHandle(0);
                     mute.removeClass('mejs-mute').addClass('mejs-unmute');
                 } else {
