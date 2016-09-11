54be17c1b7108a0b079e3c64071692f0a5858fb2
vivekannan <vivek7641@gmail.com>
2016-09-11 23:35:39 +0530

    Minor bug fixes. Appstarted event is no longer used.

--- src/js/features/playlist.js
+++ src/js/features/playlist.js
@@ -2,7 +2,7 @@
     var playTypes = ['Normal', 'Repeat', 'Shuffle'],
         playType = 0;
     
-    mejs.Utility.getFromSettings('playType', 0, function(value) {
+    mejs.Utility.storage.get('playType', 0, function(value) {
         playType = value;
     });
     
@@ -49,7 +49,7 @@
         
         chrome.contextMenus.update(playType + 'p', { 'checked': true });
         
-        mejs.Utility.setIntoSettings('playType', playType);
+        mejs.Utility.storage.set('playType', playType);
         this.notify('Playlist Navigation: ' + playTypes[playType]);
     };
     
