e4f18d5a39a729eb33662674c7759de4c9cf1fee
vivekannan <vivek7641@gmail.com>
2016-11-28 17:15:34 +0530

    Fixed lint errors.

--- src/js/features/volume.js
+++ src/js/features/volume.js
@@ -11,6 +11,19 @@
         this.gainNode.connect(audioContext.destination);
         
         var t = this,
+            mute = $('<div class="mejs-button mejs-volume mejs-mute">' +
+                        '<button type="button" title="' + t.muteText + '"></button>' +
+                        '<progress id="volumeBar" value="' + t.startVolume + '" max="' + t.maximumVolume + '"></progress>' +
+                    '</div>')
+                        .appendTo(t.rightControls),
+            volumeBar = mute.find('#volumeBar')
+                            .on('mousedown', function(e) {
+                                handleVolumeMove(e);
+                                volumeBar.on('mousemove', handleVolumeMove);
+                            })
+                            .on('mouseup mouseleave', function() {
+                                volumeBar.off('mousemove', handleVolumeMove);
+                            }),
             handleVolumeMove = function(e) {
                 var volume = null,
                     totalOffset = volumeBar.offset();
@@ -25,20 +38,7 @@
                 // ensure the volume isn't outside 0-2
                 // set the media object (this will trigger the volumechanged event)
                 t.setVolume(Math.max(0, Math.min(volume * 2, t.maximumVolume)));
-            },
-            mute = $('<div class="mejs-button mejs-volume mejs-mute">' +
-                        '<button type="button" title="' + t.muteText + '"></button>' +
-                        '<progress id="volumeBar" value="' + t.startVolume + '" max="' + t.maximumVolume + '"></progress>' +
-                    '</div>')
-                        .appendTo(t.rightControls),
-            volumeBar = mute.find('#volumeBar')
-                            .on('mousedown', function(e) {
-                                handleVolumeMove(e);
-                                volumeBar.on('mousemove', handleVolumeMove);
-                            })
-                            .on('mouseup mouseleave', function() {
-                                volumeBar.off('mousemove', handleVolumeMove);
-                            });
+            };
         
         mute.find('button')
             .on('click', function() {
